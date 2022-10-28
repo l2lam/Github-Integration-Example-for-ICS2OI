637f8c801ce29019c1985150c3a478ce76f3570a
Ly Lam <l2lam@users.noreply.github.com>
2022-10-28 12:53:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+  text("Hello world", 50, 50);
+}
+
+function draw() {
+
+}
